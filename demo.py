871a0270e133d82e05702952801df635f9dc45af
softdrink1991@gmail.com <yanetolstiy@bk.ru>
2021-11-17 23:38:17 +0300

    v1

--- demo.py
+++
@@ -1,6 +0,0 @@
-from pytapo import Tapo
-user = 'jewell'
-password = 'Jennydog14'
-host = 'jewellfamily.ddns.net/stream1'
-tapo = Tapo(host, user, password)
-print(tapo.getBasicInfo())
--- main.py
+++ main.py
@@ -2,31 +2,33 @@
 from PyQt5 import QtWidgets
 from PyQt5.QtCore import QTimer, QThread, QThreadPool, QRunnable, QObject, pyqtSignal
 from PyQt5.QtGui import QPixmap, QImage
-import rtsp
+
 import threading
 import time
 from ui import Ui_Dialog
 import sys
-
+import cv2
+import platform
 
 class RenderCamera(QObject):
     finished = pyqtSignal()
-    def __init__(self, client, label):
+    def __init__(self, capture, label):
         super().__init__()
         self.label = label
-        self.client = client
-
+        self.capture = capture
+        
     def run(self):
-        while True:
-            _frame = self.client.read(raw=True)
-            h, w, channel = _frame.shape
-            print(h)
-            bytesPerLine = 3 * w
-            qImg = QImage(_frame.data, w, h, bytesPerLine, QImage.Format_RGB888).scaled(self.label.width() - 50, self.label.height() - 50)
+        while self.capture.isOpened():
+            ret, frame = self.capture.read()
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            h, w, channel = frame.shape
+            bytesPerLine = channel * w
+            qImg = QImage(frame.data, w, h, bytesPerLine, QImage.Format_RGB888).scaled(self.label.width(), self.label.height())
             self.label.setPixmap(QPixmap(qImg))
-            time.sleep(0.001)
+            time.sleep(40)
 
         self.finished.emit()
+
 class Application(QtWidgets.QDialog):
     def __init__(self):
         super(Application, self).__init__()
@@ -34,66 +36,48 @@ class Application(QtWidgets.QDialog):
         self.ui.setupUi(self)
         self.setLayout(self.ui.gridLayout)
         self.ui.groupBox.setLayout(self.ui.horizontalLayout)
-        self.ui.pushButton.clicked.connect(self.startPlay)
-        self.count = 8
-        self.client = []
         self.workers = []
         self.threads = []
+        self.count = 8
+        self.capture = []
+        self.label = [self.ui.cam1, self.ui.cam2, self.ui.cam3, self.ui.cam4, self.ui.cam5, self.ui.cam6, self.ui.cam7, self.ui.cam8]
+
+    def readyPlay(self):
         for index in range(self.count):
-            print(index)
-            self.client.append(rtsp.Client(rtsp_server_uri='rtsp://jewell:Jennydog14@jewellfamily.ddns.net/stream2'))
-            # self.client.append(rtsp.Client(rtsp_server_uri='rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov'))
-        self.labels = [self.ui.cam1, self.ui.cam2, self.ui.cam3, self.ui.cam4, self.ui.cam5, self.ui.cam6, self.ui.cam7, self.ui.cam8]
-
-        # self.timer = QTimer()
-        # self.timer.timeout.connect(self.refreshCamera)
-        # self.timer.start(10)
-        self.refreshCamera()
-        # self.client1 = rtsp.Client(rtsp_server_uri='rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov')
-        # self.client2 = rtsp.Client(rtsp_server_uri='rtsp://freja.hiof.no:1935/rtplive/_definst_/hessdalen03.stream')
-        # self.client3 = rtsp.Client(rtsp_server_uri='rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mp4')
-
-        # rtsp://freja.hiof.no:1935/rtplive/_definst_/hessdalen02.stream
-
-        # rtsp://freja.hiof.no:1935/rtplive/_definst_/hessdalen03.stream
-    def setURL(self):
-        pass
-    def startPlay(self):
-        self.timer.start(40)
-    def refreshCamera(self):
-        ww = self.ui.cam1.width()
-        hh = self.ui.cam1.height()
-        
-        for index in range(self.count):
+            self.capture.append(cv2.VideoCapture('rtsp://jewell:Jennydog14@jewellfamily.ddns.net/stream1'))
             self.threads.append(QThread())
-            self.workers.append(RenderCamera(self.client[index], self.labels[index]))
+            self.workers.append(RenderCamera(self.capture[index], self.label[index]))
             
             self.workers[index].moveToThread(self.threads[index])
             self.threads[index].started.connect(self.workers[index].run)
             self.workers[index].finished.connect(self.workers[index].deleteLater)
             self.threads[index].finished.connect(self.threads[index].deleteLater)
             self.threads[index].start()
-        # pool = QThreadPool.globalInstance()
 
-        # ww = self.ui.cam1.width()
-        # hh = self.ui.cam2.height()
-
-        # for index in range(self.count):
-        #     runnable = RenderCamera(self.client[index], labels[index], ww, hh)
-        #     pool.start(runnable)
-
+            # self.mediaplayer.append(self.instance.media_player_new())
+            # self.media.append(self.instance.media_new('rtsp://jewell:Jennydog14@jewellfamily.ddns.net/stream1'))
         
-        # for index in range(self.count):
-        #     _image = self.client[index].read(raw=True)
-        #     h, w, channel = _image.shape
-        #     bytesPerLine = 3 * w
-        #     qImg = QImage(_image.data, w, h, bytesPerLine, QImage.Format_RGB888).scaled(ww - 50, hh - 50)
-        #     self.labels[index].setPixmap(QPixmap(qImg))
+            # self.mediaplayer[index].set_media(self.media[index])
+            # self.media[index].parse()
+            # print(self.media[index].get_meta(0))
+            # if platform.system() == "Linux": # for Linux using the X Server
+            #     self.mediaplayer[index].set_xwindow(int(self.label[index].winId()))
+            # elif platform.system() == "Windows": # for Windows
+            #     self.mediaplayer[index].set_hwnd(int(self.label[index].winId()))
+            # elif platform.system() == "Darwin": # for MacOS
+            #     self.mediaplayer[index].set_nsobject(int(self.label[index].winId()))
+            # self.mediaplayer[index].play()
+
+    def startPlay(self):
+        pass        
 
 def main():
+
     app = QtWidgets.QApplication(sys.argv)
     mainDlg = Application()
+
     mainDlg.showFullScreen()
+    mainDlg.readyPlay()
     sys.exit(app.exec_())
 
 
